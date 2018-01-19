9b417786257b5751a94c7cbc1f01fe1eed69ab5d
suryaprabhaponnusamy <35135322+suryaprabhaponnusamy@users.noreply.github.com>
2018-01-19 16:35:52 +0530

    Create odd interval.c

---
+++ odd interval.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int num,m=20,n=40;
+print("print odd numbers in a given range m to n:\n");
+scanf("%d",&num);
+for(num=m;num<=n;num++)
+{
+printf("%d",num);
+}
+return 0;
+getch();
+]
