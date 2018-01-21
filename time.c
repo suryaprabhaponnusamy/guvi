d83c60304bff2b385d22c451eaf3396547a5ab74
suryaprabhaponnusamy <35135322+suryaprabhaponnusamy@users.noreply.github.com>
2018-01-21 11:54:03 +0530

    Create time.c

---
+++ time.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+{
+int time=0,minutes=0,hours=0;
+time=130;
+minutes=time%60;
+hours=(time.minutes)/60;
+printf("total minutes%d=%dtime and minutes%d",time,hours,minutes);
+return 0;
+}
